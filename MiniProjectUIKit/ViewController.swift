55011775fd8d8aa411f3ee5d3bed86138c07f171
Vincent <31364417+vcsrng@users.noreply.github.com>
2024-12-02 09:34:25 +0700

    Initial Commit

---
+++ MiniProjectUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MiniProjectUIKit
+//
+//  Created by Vincent Saranang on 02/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MiniProjectUIKitTests/MiniProjectUIKitTests.swift
@@ -0,0 +1,17 @@
+//
+//  MiniProjectUIKitTests.swift
+//  MiniProjectUIKitTests
+//
+//  Created by Vincent Saranang on 02/12/24.
+//
+
+import Testing
+@testable import MiniProjectUIKit
+
+struct MiniProjectUIKitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
